addbb290bc932ca5cf57dc9e07990c0e8644305f
drissakande <drissakande@gmail.com>
2023-09-02 11:28:52 -0400

    Complete Basic Routes Lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,18 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+  <h1>Actors Page</h1>
+  {actors.map((actor, index)=>(<div key={index}>
+  <h2>Name: {actor.name}</h2>
+  <ul>
+  Movies: 
+    {actor.movies.map((movie, index)=>(<li key={index}>{movie}</li>))}
+  </ul>
+
+
+
+  </div>)) /*{code here}*/}</div>;
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,18 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+  <h1>Movies Page</h1>
+
+  {movies.map((movie, index)=>(<div key={index}>
+  <h2>Name: { movie.title }</h2>
+    <p>Time: { movie.time }</p>
+    <ul>
+    Genres:
+
+      {movie.genres.map((genre, index)=>(<li key={index}>{genre}</li>)) /* <li> { movie.genres } <br/> </li> */}
+    </ul>
+  </div>)) /*{code here}*/}</div>;
 }
 
 export default Movies;
